ebddc9c76db82b221e4b0bb231af5e7c75721efa
jfqd <jfqd@blun.org>
2021-08-29 18:51:38 +0200

    add exception log-output

--- lib/uploadable_asset.rb
+++ lib/uploadable_asset.rb
@@ -5,16 +5,15 @@ module UploadableAsset
   
   class String
     def umlaut_filter
-      self.gsub("ä", "ae").gsub("ö", "oe").gsub("ü", "ue").gsub("Ä", "Ae").gsub("Ö", "Oe").gsub("Ü", "Ue").gsub("ß", "ss") rescue self
+      self.gsub("ä", "ae").gsub("ö", "oe").gsub("ü", "ue").gsub("Ä", "Ae").gsub("Ö", "Oe").gsub("Ü", "Ue").gsub("ß", "ss").gsub("®", "")
+    rescue Exception => e
+      Rails.logger.warn("[UploadableAsset#umlaut_filter] Error: #{e.message}")
+      self
     end
   end
   
   def uploaded_file=(upload)
-    asset_file_name = if defined?(RadiantMagicExtension)
-      self.title.force_encoding("UTF-8").strip.umlaut_filter.gsub( /[^\w\.\-]/, '_' )
-    else
-      self.title
-    end
+    asset_file_name = self.title.force_encoding("UTF-8").strip.umlaut_filter.gsub( /[^\w\.\-]/, '_' )
     self.asset = upload
     self.asset_file_name = asset_file_name
     self.asset_file_size ||= upload.size rescue 0
