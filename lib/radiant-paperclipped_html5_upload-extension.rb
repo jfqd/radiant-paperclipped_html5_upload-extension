83a4a583322c41b350d878475e0223ccdbf6d265
jfqd <jfqd@blun.org>
2021-08-29 19:21:09 +0200

    bump version to 0.1.8

--- lib/radiant-paperclipped_html5_upload-extension.rb
+++ lib/radiant-paperclipped_html5_upload-extension.rb
@@ -1,5 +1,5 @@
 module RadiantPaperclippedHtml5UploadExtension
-  VERSION     = "0.1.7"
+  VERSION     = "0.1.8"
   SUMMARY     = "html5 asset uploader for Radiant CMS. Extends paperclipped extension"
   DESCRIPTION = "html5 asset uploader for Radiant CMS based on jquery. Extends paperclipped extension."
   URL         = "https://github.com/jfqd/radiant-paperclipped_html5_upload-extension"
